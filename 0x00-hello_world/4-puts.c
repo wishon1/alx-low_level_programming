31d0b500afb64a17b8d803559f31878f35ee313a
wishon1 <wisdomhonest1@gmail.com>
2023-06-15 21:27:36 -0700

    display text on std.o using puts()

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- *int main - start of the program
+ *main - start of the program
  *puts - display text to the standard output
- *Return - terminate the program with success
+ *Return: terminate the program with success
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
 
