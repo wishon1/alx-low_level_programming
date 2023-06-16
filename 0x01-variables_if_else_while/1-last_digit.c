351a93275b4fdd1c8bc2ed0491806b1b8b4dac39
wishon1 <wisdomhonest1@gmail.com>
2023-06-16 11:04:27 -0700

    Assign number to the variable n

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -10,19 +10,21 @@
 int main(void)
 {
 	int n;
+	int lst_dgt;/* last digit is declared by me to solve the problem */
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-	if (n > 5)
+	/* my code start here: lst_dgt is initialize */
+	lst_dgt = n % 10;
+	if (lst_dgt > 5)
 	{
-		printf("Last digit of %d is %d and is greater than 5\n", n, n);
-	} else if (n == 0)
+		printf("Last digit of %d is %d and is greater than 5\n", n, lst_dgt);
+	} else if (lst_dgt == 0)
 	{
-		printf("Last digit of %d is %d and is 0\n", n, n);
-	} else if (n < 6 && n != 0)
+		printf("Last digit of %d is %d and is 0\n", n, lst_dgt);
+	} else if (lst_dgt < 6 && lst_dgt != 0)
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lst_dgt);
 	}
 	return (0);
 }
