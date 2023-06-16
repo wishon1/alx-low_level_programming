d45b2361ff4d2140ae7f708bc1b96457522dabec
wishon1 <wisdomhonest1@gmail.com>
2023-06-15 21:20:55 -0700

    Display text to std.o using puts()

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ *int main - start of the program
+ *puts - display text to the standard output
+ *Return - terminate the program with success
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
+
