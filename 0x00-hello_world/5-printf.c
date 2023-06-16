d31092388b254047f1b52a63f17ef3d2738d5bc4
wishon1 <wisdomhonest1@gmail.com>
2023-06-15 21:38:30 -0700

    Display output using printf()

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ *main - start of the program
+ *prinf() - Display text to the std.o
+ *Return: terminate program with success
+ */
+int main(void)
+{
+	printf("\"with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
