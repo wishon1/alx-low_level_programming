e313988aa2cb4d08bfd41d13f628d2b5df201cae
wishon1 <wisdomhonest1@gmail.com>
2023-06-16 13:39:11 -0700

    Program that prints the 'alpha' in low/up

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - startof the programe
+ *
+ * Return: return 0 upon successful termination
+ */
+int main(void)
+{
+	int lowCase;
+	int upCase;
+
+	lowCase = 97;
+	upCase = 65;
+
+	while (lowCase <= 122)
+	{
+		putchar(lowCase);
+	}
+	while (upCase <= 90)
+	{
+		putchar(upCase);
+	}
+	putchar('\n');
+	return (0);
+}
