494ce7254c6f90ba62ae46a234c337b39b523ec9
wishon1 <wisdomhonest1@gmail.com>
2023-06-16 15:19:42 -0700

    prints digit num. of base 10 starting from 0

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - start the code
+ *
+ * Return: Terminate the code with success @ 0
+ */
+int main(void)
+{
+	int num = 0;
+
+	for (num = 0; num <= 9; num++)
+	{
+		printf("%d", num);
+	}
+	printf("\n");
+	return (0);
+}
