5ffbbafbed9a7e58ca6da9a4f65d791984c2d81e
wishon1 <wisdomhonest1@gmail.com>
2023-06-17 13:45:25 -0700

    print output with write(1, our_name, strlen(our_name))

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - start of the program
+ * using write(1, our_name, strlen(our_name))
+ * Return: terminate the program
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\", - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
