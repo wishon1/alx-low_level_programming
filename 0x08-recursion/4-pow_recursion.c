b04ac4ebcee408cd0b48cb1a66396f5119027929
wishon1 <wisdomhonest1@gmail.com>
2023-07-06 07:20:40 -0700

    find the power of x raise to y

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * _pow_recursion - find the squre root of x raise to power
+ * _pow_recursion - find the squre  of x raise to power
  * of y
  * @x: integer value to find its square root
  * @y: power of x
  *
- * return: return square root of x raise to y
+ * Return: return square root of x raise to y
  */
 int _pow_recursion(int x, int y)
 {
