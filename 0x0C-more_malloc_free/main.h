28cdd40d24928dac52ab3d7a6364775226b0447b
wishon1 <wisdomhonest1@gmail.com>
2023-07-14 08:19:43 -0700

    new prototype add to the header file

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,6 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
-
+#include <limits.h>
 int *array_range(int min, int max);
 void *_calloc(unsigned int nmemb, unsigned int size);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
