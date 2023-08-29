e2013737116e145b6a4a926ca6b4c884e4e2f259
wishon1 <wisdomhonest1@gmail.com>
2023-08-29 14:36:14 -0400

    frees a listint_t list.

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ * free_listint - free a list
+ * @head: pointer to the first list
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *ptr;
+
+	if (head == NULL)
+		return;
+
+	while (*head != NULL)
+	{
+		ptr = *head;
+
+		*head = (*head)->next;
+
+		free(ptr);
+	}
+	*head = NULL;
+}
+
