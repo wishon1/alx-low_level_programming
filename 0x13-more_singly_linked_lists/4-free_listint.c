2004177605e7b028f03a2207c1d2143736ef93c2
wishon1 <wisdomhonest1@gmail.com>
2023-08-29 14:19:43 -0400

    free a node

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -5,18 +5,12 @@
  */
 void free_listint(listint_t *head)
 {
-	listint_t *current_node = head;
-	listint_t *next_node;
+	listint_t *ptr;
 
-	while (current_node->next != NULL)
+	while (head != NULL)
 	{
-		/** save the next node before free the current node */
-		next_node = current_node->next;
-
-		/** free the current node */
-		free(current_node);
-
-		/** move to the next node */
-		current_node = next_node;
+		ptr = head;
+		head = head->next;
+		free(ptr);
 	}
 }
