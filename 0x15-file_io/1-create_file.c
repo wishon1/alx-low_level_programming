e70fcf1b3f3b2aa2f521bb349e0954549dca9c3f
wishon1 <wisdomhonest1@gmail.com>
2023-09-04 21:17:14 -0400

    function that creates a file.

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -25,7 +25,7 @@ int create_file(const char *filename, char *text_content)
 		text_content = "";
 
 	content_length = 0;
-	while (text_content[content_length])
+	while (text_content[content_length] != '\0')
 		content_length++;
 
 	bytes_written = write(fd, text_content, content_length);
