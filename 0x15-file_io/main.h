2230ef06925887bc4fb90ef1ddd714a422743710
wishon1 <wisdomhonest1@gmail.com>
2023-09-05 08:58:46 -0400

    copies the content of a file to another file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,12 +5,9 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
-#include <string.h>
-#include <errno.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 
-void handle_file_error(int file_from, int file_to, char *argv[]);
 int _putcahr(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
