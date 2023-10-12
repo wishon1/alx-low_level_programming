6f032a3f6006e3d45365752b0a77fda26441f68b
wishon1 <wisdomhonest1@gmail.com>
2023-10-12 16:54:50 -0400

    returns the nth node of a dlistint_t linked list.

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -16,7 +16,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	if (head == NULL)
 		return (NULL);
 
-	while (head != NULL)
+	while (ptr != NULL)
 	{
 		if (count == index)
 			return (ptr);
